1269ae7b5ece739c655b703240045efbb5e52621
Pratik Yesare <pratik.yesare68@gmail.com>
2025-05-11 16:33:25 +0530

    Added Homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-export default function HomePage() {
+const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Home Page
+    <div className="home-wrapper">
+      <div className="home-content">
+        <h1>
+          Welcome to <span className="brand-name">Shopify</span>
+        </h1>
+        <p className="subtitle">
+          Discover quality fashion, top electronics, and exclusive deals—all in
+          one place.
+        </p>
+        <button onClick={() => navigate("/products")} className="explore-btn">
+          Explore Products
+        </button>
+      </div>
+      <div className="home-illustration">
+        <DotLottieReact
+          src="https://lottie.host/1e74b3ba-710b-4086-bd3a-2579a46a2550/A4Gh0WI0GB.lottie"
+          loop
+          autoplay
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
